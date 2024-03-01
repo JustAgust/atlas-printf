6133cca93741a94afa6528b81d2796cf9087eadd
JustAgust <charles.sparks@atlasschool.com>
2024-03-01 19:17:32 +0000

    changed main.c file for test

--- main.c
+++ main.c
@@ -13,6 +13,22 @@ int main(void)
     int len2;
     unsigned int ui;
     void *addr;
+/**betty compliance!*/
+    _printf("%s", "This sentence is retrieved from va_args!\n");
+  _printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+_printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+_printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+_printf("css%ccs%scscscs", 'T', "Test");
+ _printf(str);
+ _printf("man gcc:\n%s", str);
+_printf("%");
+_printf("%!\n");
+_printf("%K\n");
+
+
+
+
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
