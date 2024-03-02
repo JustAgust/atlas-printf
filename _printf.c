3a6b9e53846a297c403031034669f865f6141641
JustAgust <charles.sparks@atlasschool.com>
2024-03-02 17:33:32 +0000

    Revert "removed lines 21-24 modified strcat section of printf"
    This reverts commit e6a091a199045d49cc58930924adc040b6b19906.

--- _printf.c
+++ _printf.c
@@ -18,7 +18,10 @@ int _printf(const char *format, ...)
 	if (os == NULL)
 		return (-1);
 	va_start(parm, format);
-
+	/**
+	 * i = 0;
+	 * j = 0;
+	 */
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
@@ -31,32 +34,25 @@ int _printf(const char *format, ...)
 			}
 			else if (format[i] == 's')
 			{
-				char *arg = va_arg(parm, char *);
-				if (arg == NULL)
-				{
-					_strcat(os, "(null)";
-							}
-							else
-							{
-							_strct(os, arg);
-							}
-							j = _strlen(os) - 1;
-							}
-							else if (format[i] == '%')
-							os[j] = '%';
-							}
-							else
-							{
-							(os[j] = format[i]);
-							}
-							i++;
-							j++;
-							}
-							os[j] = '\0';
-							write(1, os, j);
-							va_end(parm);
-							free(os);
-							return (j);
+				os[j] = '\0';
+				_strcat(os, va_arg(parm, char *));
+				j = _strlen(os) - 1;
+			}
+			else if (format[i] == '%')
+				os[j] = '%';
+		}
+		else
+		{
+			(os[j] = format[i]);
+		}
+		i++;
+		j++;
+	}
+	os[j] = '\0';
+	write(1, os, j);
+	va_end(parm);
+	free(os);
+	return (j);
 }
 
 
