e6a091a199045d49cc58930924adc040b6b19906
JustAgust <charles.sparks@atlasschool.com>
2024-03-02 17:09:53 +0000

    removed lines 21-24 modified strcat section of printf

--- _printf.c
+++ _printf.c
@@ -18,10 +18,7 @@ int _printf(const char *format, ...)
 	if (os == NULL)
 		return (-1);
 	va_start(parm, format);
-	/**
-	 * i = 0;
-	 * j = 0;
-	 */
+
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
@@ -34,25 +31,32 @@ int _printf(const char *format, ...)
 			}
 			else if (format[i] == 's')
 			{
-				os[j] = '\0';
-				_strcat(os, va_arg(parm, char *));
-				j = _strlen(os) - 1;
-			}
-			else if (format[i] == '%')
-				os[j] = '%';
-		}
-		else
-		{
-			(os[j] = format[i]);
-		}
-		i++;
-		j++;
-	}
-	os[j] = '\0';
-	write(1, os, j);
-	va_end(parm);
-	free(os);
-	return (j);
+				char *arg = va_arg(parm, char *);
+				if (arg == NULL)
+				{
+					_strcat(os, "(null)";
+							}
+							else
+							{
+							_strct(os, arg);
+							}
+							j = _strlen(os) - 1;
+							}
+							else if (format[i] == '%')
+							os[j] = '%';
+							}
+							else
+							{
+							(os[j] = format[i]);
+							}
+							i++;
+							j++;
+							}
+							os[j] = '\0';
+							write(1, os, j);
+							va_end(parm);
+							free(os);
+							return (j);
 }
 
 
