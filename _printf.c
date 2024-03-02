d3a41587f9c47464e4f8559b95c7c404d97ec566
JustAgust <charles.sparks@atlasschool.com>
2024-03-02 01:43:29 +0000

    reverted changes to last known best

--- _printf.c
+++ _printf.c
@@ -31,8 +31,8 @@ int _printf(const char *format, ...)
 			else if (format[i] == 's')
 			{
 				/**os[j] = '\0';*/
-				_strcat(os, char *);
-				j += _strlen(os);
+				_strcat(os, va_arg(parm, char *));
+				j = _strlen(os) - 1;
 			}
 			else if (format[i] == '%')
 				os[j] = '%';
