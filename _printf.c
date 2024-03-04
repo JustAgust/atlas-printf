bb1de9ae64bd950ebf6af9e6352237fd9269afc5
JustAgust <charles.sparks@atlasschool.com>
2024-03-04 17:58:59 +0000

    still working on John Wick 3

--- _printf.c
+++ _printf.c
@@ -37,28 +37,26 @@ int _printf(const char *format, ...)
 				os[j] = '\0';
 				args = va_arg(parm, char *);
 				if (args == NULL)
-					args = "(null)";
+					_strcat(os, "(null)");
+				else
+					_strcat(os, args);
+				j = _strlen(os) - 1;
 			}
-
-			
-			_strcat(os, args);
-			j = _strlen(os) - 1;
+			else if (format[i] == '%')
+				os[j] = '%';
 		}
-		else if (format[i] == '%')
-			os[j] = '%';
+		else
+		{
+			(os[j] = format[i]);
+		}
+		i++;
+		j++;
 	}
-	else
-	{
-		(os[j] = format[i]);
-	}
-	i++;
-	j++;
-}
-os[j] = '\0';
-write(1, os, j);
-va_end(parm);
-free(os);
-return (j);
+	os[j] = '\0';
+	write(1, os, j);
+	va_end(parm);
+	free(os);
+	return (j);
 }
 
 
